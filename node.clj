01f1a969bc350f035dfca454a1760e033a2691e9
Dekel Pilli <dekelpilli@gmail.com>
2022-03-12 19:18:09 +1100

    Structure

---
+++ node.clj
@@ -0,0 +1,6 @@
+(require 'cljs.build.api)
+
+(cljs.build.api/build "src"
+                      {:main 'campaign3.core
+                       :output-to "main.js"
+                       :target :nodejs})
