3dd01b9a12b936aecee6665011ce28b3bff602d8
Dekel Pilli <dekelpilli@gmail.com>
2022-04-17 02:51:40 +1000

    Make realias! reload all aliases in namespace dynamically

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,6 @@
 (ns user
   (:require [clojure.tools.namespace.repl :as repl]
+            [clojure.pprint :refer [pprint pp]]
             [hawk.core :as hawk]
             [campaign3
              [util :as u]
@@ -17,30 +18,14 @@
              [riddle :as riddle]
              [core :as core]]))
 
-(def reqs '[[campaign3.util u]
-            [campaign3.prompting p]
-            [campaign3.relics relics]
-            [campaign3.mundanes mundane]
-            [campaign3.enchants e]
-            [campaign3.crafting crafting]
-            [campaign3.consumables consumables]
-            [campaign3.prayers prayers]
-            [campaign3.encounters encounter]
-            [campaign3.dice dice]
-            [campaign3.rings rings]
-            [campaign3.uniques unique]
-            [campaign3.riddle riddle]
-            [campaign3.core core]])
-
 (defmacro realias! []
   (cons 'do
-        (reduce (fn [l [ns-sym alias-sym]]
-                  (cons (list `ns-unalias `*ns* `'~alias-sym)
-                        (cons
-                          (list `alias `'~alias-sym `'~ns-sym)
-                          l)))
+        (reduce (fn [l [alias-sym ns-sym]]
+                  (->> l
+                       (cons (list `alias `'~alias-sym `'~ns-sym))
+                       (cons (list `ns-unalias `*ns* `'~alias-sym))))
                 '()
-                reqs)))
+                (update-vals (ns-aliases *ns*) (comp symbol str)))))
 
 (hawk/watch! [{:paths   ["src"]
                :handler (fn [ctx e]
