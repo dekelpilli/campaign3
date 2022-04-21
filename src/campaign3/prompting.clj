69ffdb10d52a32d9c5f4ccc83030ec2c7f0d00bf
Dekel Pilli <dekelpilli@gmail.com>
2022-04-22 00:46:14 +1000

    Improve handling of checkbox for maps, use input w/ completer instead of lists when >10 options

--- src/campaign3/prompting.clj
+++ src/campaign3/prompting.clj
@@ -1,24 +1,32 @@
 (ns campaign3.prompting
-  (:require [clojure.walk :as walk])
-  (:import (de.codeshelf.consoleui.prompt ConsolePrompt CheckboxResult InputResult ListResult)))
+  (:require [clojure.string :as str])
+  (:import (de.codeshelf.consoleui.prompt ConsolePrompt CheckboxResult InputResult ListResult)
+           (jline.console.completer StringsCompleter)
+           (java.util Collection)))
 
 (def ^:private console-prompt (ConsolePrompt.))
+
+;TODO custom completer for comma-separated input version of checkbox
 
 (defn- stringify [x]
   (if (keyword? x) (name x) (str x)))
 
 (defn- stringify-keys [{:keys [sorted?]} m]
-  (walk/postwalk (fn [x] (if (map? x)
-                           (into (if sorted? (sorted-map) {})
-                                 (map (juxt (comp stringify key) val))
-                                 x)
-                           x))
-                 m))
+  (into (if sorted? (sorted-map) {})
+        (map (juxt (comp stringify key) val))
+        m))
+
+(defn- ->stringified-map [coll opts]
+  (if (map? coll)
+    (stringify-keys opts coll)
+    (into (if sorted? (sorted-map) {}) (map (juxt stringify identity)) coll)))
 
 (defn >>checkbox
   ([coll] (>>checkbox "Choose all that apply:" coll))
-  ([prompt coll]
-   (let [prompt-builder (.getPromptBuilder console-prompt)]
+  ([prompt coll & {:keys [sorted?]
+                   :as   opts}]
+   (let [prompt-builder (.getPromptBuilder console-prompt)
+         m (->stringified-map coll opts)]
      (-> prompt-builder
          (.createCheckboxPrompt)
          (.message prompt)
@@ -27,46 +35,53 @@
                                        (.text %2)
                                        (.add)))
                          builder
-                         (map stringify coll)))
+                         (keys m)))
          (.addPrompt))
      (-> (.prompt console-prompt (.build prompt-builder))
          ^CheckboxResult (get prompt)
          (.getSelectedIds)
-         (set)))))
-
-(defn >>item
-  ([coll] (>>item "Choose one from these:" coll))
-  ([prompt coll & {:keys [sorted?]
-                   :as   opts}]
-   ;TODO when (count coll) is above a certain number (10?), use input + autocomplete instead of list
-   (let [prompt-builder (.getPromptBuilder console-prompt)
-         m (if (map? coll)
-             (stringify-keys opts coll)
-             (into (if sorted? (sorted-map) {}) (map (juxt stringify identity)) coll))]
-     (-> prompt-builder
-         (.createListPrompt)
-         (.message prompt)
-         (as-> builder (reduce
-                         #(doto %1 (-> (.newItem %2)
-                                       (.text %2)
-                                       (.add)))
-                         builder
-                         (-> m (keys) (conj "\u001B[31mNone\u001B[0m"))))
-         (.addPrompt))
-     (-> (.prompt console-prompt (.build prompt-builder))
-         ^ListResult (get prompt)
-         (.getSelectedId)
-         (m)))))
+         (->> (into (if sorted? (sorted-set) #{}) (map m)))))))
 
 (defn >>input
   ([] (>>input "Enter text: "))
-  ([prompt]
+  ([prompt] (>>input prompt nil))
+  ([prompt valid-inputs]
    (let [prompt-builder (.getPromptBuilder console-prompt)]
      (-> prompt-builder
          (.createInputPrompt)
          (.message prompt)
          (.name prompt)
+         (cond-> valid-inputs (.addCompleter (StringsCompleter. ^Collection valid-inputs)))
          (.addPrompt))
      (-> (.prompt console-prompt (.build prompt-builder))
          ^InputResult (get prompt)
          (.getInput)))))
+
+(defn >>item
+  ([coll] (>>item "Choose one from these:" coll))
+  ([prompt coll & {:keys [sorted?]
+                   :as   opts}]
+   (let [prompt-builder (.getPromptBuilder console-prompt)
+         m (if (map? coll)
+             (stringify-keys opts coll)
+             (into (if sorted? (sorted-map) {}) (map (juxt stringify identity)) coll))]
+     (if (> (count m) 10)
+       (some->> (keys m)
+                (>>input prompt)
+                (str/trimr)
+                (get m))
+       (do
+         (-> prompt-builder
+             (.createListPrompt)
+             (.message prompt)
+             (as-> builder (reduce
+                             #(doto %1 (-> (.newItem %2)
+                                           (.text %2)
+                                           (.add)))
+                             builder
+                             (-> m (keys) (conj "\u001B[31mNone\u001B[0m"))))
+             (.addPrompt))
+         (-> (.prompt console-prompt (.build prompt-builder))
+             ^ListResult (get prompt)
+             (.getSelectedId)
+             (m)))))))
