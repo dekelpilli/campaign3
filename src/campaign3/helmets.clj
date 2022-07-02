144396924364ca026b5817e0b2f2d82a19efcffa
Dekel Pilli <dekelpilli@gmail.com>
2022-07-03 01:42:46 +1000

    Divinity tweaks, add fn for upgrading helms

--- src/campaign3/helmets.clj
+++ src/campaign3/helmets.clj
@@ -2,7 +2,8 @@
   (:require [campaign3.db :as db]
             [campaign3.prompting :as p]
             [randy.core :as r]
-            [campaign3.enchants :as e]))
+            [campaign3.enchants :as e]
+            [campaign3.util :as u]))
 
 (def character-enchants (->> (db/load-all :character-enchants)
                              (group-by :character)))
@@ -10,3 +11,16 @@
 (defn new []
   (when-let [enchants (p/>>item "Character name:" character-enchants)]
     (e/add-enchants 20 #(r/sample enchants))))
+
+(defn upgrade []
+  (when-let [available-enchants (p/>>item "Character name:" character-enchants)]
+    (when-let [present-enchants (seq (p/>>distinct-items "Present enchants:" available-enchants))]
+      (when-let [enchant-levels (into {}
+                                      #(parse-long (p/>>input (str "What is the level of '" (:effect %) "'")))
+                                      present-enchants)]
+        (let [upgraded-enchant (r/sample present-enchants)
+              points-total (reduce-kv (fn [total level {:keys [points]}] (+ total (* level points)))
+                                      10 enchant-levels)
+              fractured? (u/occurred? (/ 10 points-total))]
+          {:upgraded   upgraded-enchant
+           :fractured? fractured?})))))
