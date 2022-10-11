d195f51f333a5f1673adb5b04bef1aa86d88202f
Dekel Pilli <dekelpilli@gmail.com>
2022-10-11 20:25:43 +1100

    Apply weightings individually, remove sub-1CR amulets

--- src/campaign3/randoms.clj
+++ src/campaign3/randoms.clj
@@ -1,18 +1,20 @@
 (ns campaign3.randoms
   (:require [randy.core :as r]))
 
-(defmulti randoms-preset (comp keyword :preset))
-
 (defn- keyword-type [{:keys [type] :as conf}]
   (cond-> conf
           (some? type) (update :type keyword)))
 
-(def random->values-vec (comp randoms-preset keyword-type))
+(defmulti ^:private randoms-preset (comp keyword :preset))
+(def ^:private random->values-vec (comp randoms-preset keyword-type))
+
+(defmulti ^:private randoms-factor (comp keyword :preset))
+(def ^:private random->weighting (comp randoms-factor keyword-type))
 
 (defn- sample-fn [vs]
   (fn sample-random [] (r/sample vs)))
 
-(defn random->fn [random]
+(defn- random->fn [random]
   (-> random random->values-vec sample-fn))
 
 (defn randoms->fn [randoms]
@@ -20,27 +22,18 @@
     (vector? randoms) (apply juxt (map random->fn randoms))
     (map? randoms) (random->values-vec randoms)))
 
-(defn- random->weighting-multiplier [{:keys [preset] :as random}]
-  (case preset
-    :without-replacement (* (:amount random) (random->weighting-multiplier random))
-    (count (random->values-vec random))))
-
 (defn randoms->weighting-multiplier [randoms]
-  (let [options-factor (cond
-                         (vector? randoms) (transduce (map random->weighting-multiplier) + 0 randoms)
-                         (map? randoms) (random->weighting-multiplier randoms)
-                         (nil? randoms) 1)]
-    (-> options-factor
-        double
-        Math/sqrt
-        (+ (/ options-factor 2))
-        (/ 2)
-        (max 1))))
+  (cond
+    (vector? randoms) (transduce (map random->weighting) + 0 randoms)
+    (map? randoms) (random->weighting randoms)
+    (nil? randoms) 1))
 
+(defmethod randoms-factor :languages [_] 2)
 (defmethod randoms-preset :languages [_]
   ["Common" "Dwarvish" "Elvish" "Giant" "Gnomish" "Goblin" "Halfling" "Orc"
    "Abyssal" "Celestial" "Draconic" "Deep Speech" "Infernal" "Primordial" "Sylvan" "Undercommon"])
 
+(defmethod randoms-factor :feats [_] 1)
 (defmethod randoms-preset :feats [_]
   ["Alert" "Athlete" "Actor" "Blinktouched" "Brawler" "Charger" "Crossbow Expert" "Defensive Duelist" "Dual Wielder"
    "Dungeon Delver" "Durable" "Eldritch Adept" "Empathic" "Grappler" "Fighting Initiate" "Great Weapon Master"
@@ -49,6 +42,12 @@
    "Resilient" "Ritual Caster" "Sentinel" "Sharpshooter" "Shield Master" "Skilled" "Skulker" "Specialist"
    "Spell Touched" "Summoner" "Survivor" "Tactician" "Telekinetic" "Telepathic" "War Caster" "Warlord"])
 
+(defmethod randoms-factor :skills [{:keys [type]
+                                    :or   {type :all}}]
+  (case type
+    :common 2
+    :uncommon 3
+    :all 5))
 (defmethod randoms-preset :skills [{:keys [type]
                                     :or   {type :all}}]
   (case type
@@ -59,9 +58,13 @@
           "arcana" "athletics" "acrobatics" "sleight of hand" "stealth" "history"
           "nature" "religion" "animal handling" "intimidation" "performance"]))
 
-(defmethod randoms-preset :range [{:keys [args]}]
-  (vec (apply range args)))
-
+(defmethod randoms-factor :damage-types [{:keys [type]
+                                          :or   {type :all}}]
+  (case type
+    :physical 1
+    :non-physical 2
+    :elemental 1
+    :all 3))
 (defmethod randoms-preset :damage-types [{:keys [type]
                                           :or   {type :all}}]
   (case type
@@ -70,6 +73,12 @@
     :elemental ["cold" "fire" "lightning"]
     :all ["acid" "bludgeoning" "cold" "fire" "force" "lightning" "necrotic" "piercing" "poison" "psychic" "radiant" "slashing" "thunder"]))
 
+(defmethod randoms-factor :ability-scores [{:keys [type]
+                                            :or   {type :all}}]
+  (case type
+    :common 2
+    :uncommon 2
+    :all 4))
 (defmethod randoms-preset :ability-scores [{:keys [type]
                                             :or   {type :all}}]
   (case type
@@ -77,10 +86,12 @@
     :uncommon ["Strength" "Intelligence" "Charisma"]
     :all ["Charisma" "Constitution" "Dexterity" "Intelligence" "Strength" "Wisdom"]))
 
+(defmethod randoms-factor :monster-types [_] 1)
 (defmethod randoms-preset :monster-types [_]
   ["Abberation" "Beast" "Celestial" "Construct" "Dragon" "Elemental" "Fey"
    "Fiend" "Giant" "Humanoid" "Monstrosity" "Ooze" "Plant" "Undead"])
 
+(defmethod randoms-factor :cantrips [_] 2)
 (defmethod randoms-preset :cantrips [_]
   ["Acid Splash" "Altered Strike" "Arcane Muscles" "Blade Ward" "Booming Blade" "Calculate" "Chill Touch"
    "Circular Breathing" "Control Flames" "Create Bonfire" "Dancing Lights" "Druidcraft" "Eldritch Blast"
@@ -91,8 +102,10 @@
    "Spare the Dying" "Sword Burst" "Thaumaturgy" "Thorn Whip" "Thunderclap" "Toll the Dead"
    "Tree Heal" "True Strike" "Vicious Mockery" "Word of Radiance"])
 
+(defmethod randoms-factor :literal [{:keys [factor] :or {factor 1}}] factor)
 (defmethod randoms-preset :literal [{:keys [values]}] values)
 
+(defmethod randoms-factor :without-replacement [{:keys [amount from]}] (max amount (randoms-factor from)))
 (defmethod randoms-preset :without-replacement [{:keys [amount from]}]
   (let [vs (random->values-vec from)]
     #(r/sample-without-replacement amount vs)))
